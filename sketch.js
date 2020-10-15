d074fc30f527253762447314ff53c7c3166b5173
Aryan <Aryanhotwani444@gmail.com>
2020-10-15 17:58:28 -0500

    created chain class

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
 
 var engine, world;
 var box1, pig1;
@@ -36,6 +38,10 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log = new Log(230,180,80,PI/2);
+
+    chain = new chain(bird.body,log.body)
+
 }
 
 function draw(){
@@ -61,4 +67,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log.display();
+
+    chain.display();
 }
